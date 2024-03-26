742007c8cea496906efc65a3c8ef743d67b45997
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-03-26 00:00:49 -0700

    Changes done

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,49 @@
 import { client } from "@/sanity/lib/client";
-//import Image from "next/image";
+import Image from "next/image";
+import { urlForImage } from "@/sanity/lib/image";
 
-const getServicesFromSanity = async () => {
-  const services = await client.fetch('*[_type=="services"]');
-  return services;
+const getBlogFromSanity = async () => {
+  const blog = await client.fetch(
+    '*[_type=="blog"]',
+    {},
+    {
+      cache: "no-cache",
+    }
+  );
+
+  return blog;
 };
+getBlogFromSanity();
 
 export default async function Home() {
-  const services = await getServicesFromSanity();
-  // console.log("nasirResponse", services);
-  // console.log("nasir");
+  const blogs = await getBlogFromSanity();
+  // console.log("nasirData", blogs);
+
   return (
-    <>
-      <p>Get data from sanity</p>
-      {services.map((item: any, ind: any) => {
-        return (
-          <div key={ind}>
-            Title:{item.title}
-            descrtiption:{item.description}
-            <hr />
-          </div>
-        );
-      })}
-    </>
+    <div>
+      <div className="gap-3 border  flex">
+        {blogs.map((item: any, index: any) => {
+          return (
+            <div key={index} className="border w-1/3 mt-10 ">
+              <div className="bg-slate-600 py-7 ">
+                <Image
+                  src={urlForImage(item.image).url()}
+                  className="w-36 h-36 mx-auto rounded-full bg-slate-500 "
+                  width={100}
+                  height={100}
+                  alt={item.image.alt}
+                />
+              </div>
+              <div className="px-4">
+                <div className="mt-3 text-2xl text-center"> {item.title}</div>
+
+                <br />
+                <div>{item.description}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
